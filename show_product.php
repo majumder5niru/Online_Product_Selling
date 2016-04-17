a981bf9de9bff2f5d666479e8811b82c2ca54856
majumderNirupam <niru.nstu@gmail.com>
2016-04-17 19:27:18 +0600

    unnecessary code has been removed for making code simpler

--- show_product.php
+++ show_product.php
@@ -1,43 +1,45 @@
+<form action="buy.php" method="post">
+	<table class="table table-bordered table-condensed table-striped table-hover" id="tableData">
+		<thead>
+			<tr>
+				<th>SL.</th>
+				<th>Product Name</th>
+				<th>Image</th>
+				<th>Price</th>
+				<th>Description</th>
+				<th>Buy</th>
+			</tr>
+		</thead>
+		<?php
+		include 'Library/dbconnect.php';
+		$res = mysql_query("SELECT product_id,product_name,product_image,price,description FROM products");
+		$i = 1;
+		while ($row = mysql_fetch_array($res))
+		{
+				
+				$s = $row['product_image'];
+				header("Content-type: image/jpeg");
+				//echo $row['product_image'];
 
-<table class="table table-bordered table-condensed table-striped table-hover" id="tableData">
-	<thead>
+		?>
 		<tr>
-			<th>SL.</th>
-			<th>Product Name</th>
-			<th>Image</th>
-			<th>Price</th>
-			<th>Description</th>
-			<th>Edit</th>
+			<td><?php echo $i;?></td>
+			<td><?php echo $row['product_name'];?></td>
+			<td><img src="data:image/jpeg;base64,<?php echo base64_encode($s); ?>" /></td>
+			<td><?php echo $row['price'];?></td>
+			<td><?php echo $row['description'];?></td>
+			<td>
+				<button class="btn btn-primary btn-xs" onclick="btn2(<?php echo $row['product_id'];?>)" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Buy</button>
+			</td>
 		</tr>
-	</thead>
-	<?php
-	include 'Library/dbconnect.php';
-	$res = mysql_query("SELECT product_id,product_name,product_image,price,description FROM products");
-	$i = 1;
-	while ($row = mysql_fetch_array($res))
-	{
-			
-			$s = $row['product_image'];
-			header("Content-type: image/jpeg");
-			//echo $row['product_image'];
+		
+		<?php
+		$i++;
+		}
+		?>	
+	</table>
+</form>
 
-	?>
-	<tr>
-		<td><?php echo $i;?></td>
-		<td><?php echo $row['product_name'];?></td>
-		<td><img src="data:image/jpeg;base64,<?php echo base64_encode($s); ?>" /></td>
-		<td><?php echo $row['price'];?></td>
-		<td><?php echo $row['description'];?></td>
-		<td>
-			<button class="btn btn-primary btn-xs" onclick="btn2(<?php echo $row['product_id'];?>)" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Buy</button>
-		</td>
-	</tr>
-	
-	<?php
-	$i++;
-	}
-	?>	
-</table>
 
 <script type="text/javascript">
 	
