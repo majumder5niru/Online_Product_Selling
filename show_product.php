c88b03e43c21c44169e21fd0c825fa28b523b640
majumderNirupam <niru.nstu@gmail.com>
2016-04-17 20:08:44 +0600

    buy_product.php file has been deleted for making code simple and passing product_id to the next page

--- show_product.php
+++ show_product.php
@@ -1,45 +1,62 @@
-<form action="buy.php" method="post">
-	<table class="table table-bordered table-condensed table-striped table-hover" id="tableData">
-		<thead>
+<?php
+	include "header_page.php";
+	include "footer_page.php";
+	include 'Library/dbconnect.php';
+	include "navbar.php";
+	session_start();
+	error_reporting(0);
+	if (!isset($_SESSION['SUserID'])) {
+		header("Location: login.php");
+		exit;
+	}
+?>
+<html>
+	<body>
+		<center><h3>You can Buy this product.</h3></center>
+		<table class="table table-bordered table-condensed table-striped table-hover" id="tableData">
+			<thead>
+				<tr>
+					<th>SL.</th>
+					<th>Product Name</th>
+					<th>Image</th>
+					<th>Price</th>
+					<th>Description</th>
+					<th>Buy</th>
+				</tr>
+			</thead>
+			<?php
+			
+			$res = mysql_query("SELECT product_id,product_name,product_image,price,description FROM products");
+			$i = 1;
+			while ($row = mysql_fetch_array($res))
+			{
+					
+					//$s = $row['product_image'];
+					//header("Content-type: image/jpeg");
+					//echo $row['product_image'];
+
+			?>
 			<tr>
-				<th>SL.</th>
-				<th>Product Name</th>
-				<th>Image</th>
-				<th>Price</th>
-				<th>Description</th>
-				<th>Buy</th>
+				<td><?php echo $i;?></td>
+				<td><?php echo $row['product_name'];?></td>
+				<td><img src="data:image/jpeg;base64,<?php echo base64_encode($s); ?>" /></td>
+				<td><?php echo $row['price'];?></td>
+				<td><?php echo $row['description'];?></td>
+				<td>
+					<form action="buy.php" method="post">
+					<button class="btn btn-primary btn-xs" > Buy</button>
+					<input type="hidden" name="product_id" value="<?php echo $product_id;?>">
+					</form>
+				</td>
 			</tr>
-		</thead>
-		<?php
-		include 'Library/dbconnect.php';
-		$res = mysql_query("SELECT product_id,product_name,product_image,price,description FROM products");
-		$i = 1;
-		while ($row = mysql_fetch_array($res))
-		{
-				
-				$s = $row['product_image'];
-				header("Content-type: image/jpeg");
-				//echo $row['product_image'];
-
-		?>
-		<tr>
-			<td><?php echo $i;?></td>
-			<td><?php echo $row['product_name'];?></td>
-			<td><img src="data:image/jpeg;base64,<?php echo base64_encode($s); ?>" /></td>
-			<td><?php echo $row['price'];?></td>
-			<td><?php echo $row['description'];?></td>
-			<td>
-				<button class="btn btn-primary btn-xs" onclick="btn2(<?php echo $row['product_id'];?>)" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Buy</button>
-			</td>
-		</tr>
-		
-		<?php
-		$i++;
-		}
-		?>	
-	</table>
-</form>
-
+			
+			<?php
+			$i++;
+			}
+			?>	
+		</table>
+	</body>
+</html>
 
 <script type="text/javascript">
 	
@@ -51,22 +68,4 @@
 	        	});
 	    });
 	
-</script>
-<script type="text/javascript">
-	function btn2(product_id) {
-	    var mode = '2';
-	    $.ajax({
-	        type: "POST",
-	        url: "product_modal.php",
-	        data: {
-	            mode: mode,
-	            product_id : product_id,
-	        },
-	        success: function (response)
-            {   
-                //alert ('edit');
-                $( '.modal-content' ).html(response);
-            }
-	    });
-	}
 </script>
