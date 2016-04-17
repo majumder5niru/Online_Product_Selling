4db4e15d5ad01ed03bf461eeecffa06ad87a417a
majumderNirupam <niru.nstu@gmail.com>
2016-04-17 22:28:53 +0600

    Some text has been added into home page

--- home.php
+++ home.php
@@ -1,3 +1,12 @@
 <?php
 	include "navbar.php";
 ?>
+<html>
+	<head>
+		<title>Online Product Selling</title>
+	</head>
+	<body>
+		<center><h3 style="padding-bottom:50px;padding-top:100px;">Welcome to Online Product Selling</h3></center>
+		<center><h4>Click on Buy Product for Buying your desired product.</h4></center>
+	</body>
+</html>
