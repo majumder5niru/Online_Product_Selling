ba744ae629da7c11a87be8b5b2af5649e6cafceb
majumderNirupam <niru.nstu@gmail.com>
2016-04-17 22:39:29 +0600

    sold product which can be viewed only by Admin was generated by making join between products table and sold table

--- sold_product.php
+++ sold_product.php
@@ -1,8 +1,76 @@
 <?php
-	$first_name = $_POST['first-name'];
-	$last_name = $_POST['last-name'];
-	$email = $_POST['email'];
-	$card_number = $_POST['card-number'];
+	session_start();
+	include "header_page.php";
+	include "footer_page.php";
+	include "navbar.php";
 
-	$Asql = mysql_query("INSERT INTO sold (first_name,last_name,email,card_number) VALUES ('$first_name','$last_name','$email','$card_number')");
+	if (!isset($_SESSION['SUserID'])) {
+		header("Location: login.php");
+		exit;
+	}
+	if($_SESSION['SUserName']=='Admin'){ ?>
+		<table class="table table-bordered table-condensed table-striped table-hover" id="tableData">
+			<thead>
+				<tr>
+					<th>SL.</th>
+					<th>Product Name</th>
+					<th>Image</th>
+					<th>Price</th>
+					<th>Customer</th>
+					<th>Email</th>
+					<th>Card Number</th>
+				</tr>
+			</thead>
+			<?php
+			include 'Library/dbconnect.php';
+			$res = mysql_query("SELECT 
+									products.product_name,
+									products.price,
+									products.product_image,
+									sold.first_name,
+									sold.email,
+									sold.card_number
+									FROM products
+									LEFT JOIN sold ON  sold.product_id = products.product_id");
+			$i = 1;
+			while ($row = mysql_fetch_array($res))
+			{
+					$s = $row['product_image'];
+			?>
+			<tr>
+				<td><?php echo $i;?></td>
+				<td><?php echo $row['product_name'];?></td>
+				<td><img src="data:image/jpeg;base64,<?php echo base64_encode($s); ?>" /></td>
+				<td><?php echo $row['price'];?></td>
+				<td><?php echo $row['first_name'];?></td>
+				<td><?php echo $row['email'];?></td>
+				<td><?php echo $row['card_number'];?></td>
+			</tr>
+			
+			<?php
+				$i++;
+			}
+			?>	
+		</table>
+	<?php
+	}else{
+		echo "You don't have access to this page. Please Log in as Admin.";
+	}
 ?>
+
+
+<script type="text/javascript">
+	
+	    $(document).ready(function() {
+	        $('#tableData').DataTable(
+	        	{
+	        		"bFilter": false,
+        			"bLengthChange": false
+	        	});
+	    });
+	
+</script>
+
+
+
+  
