8d6cc4e9cb596684741e8bf95181b8e30c3e9e60
majumderNirupam <niru.nstu@gmail.com>
2016-04-17 22:32:48 +0600

    Contact  Us link is removed from navbar

--- navbar.php
+++ navbar.php
@@ -14,6 +14,7 @@
         <li><a href = "update_profile.php">Update Profile</a></li>
         <li><a href = "product_details.php">Product Details</a></li>
         <li><a href = "show_product.php">Buy Product</a></li>
+        <li><a href = "sold_product.php">Sold Product</a></li>
       </ul> 
       <ul class="nav navbar-nav navbar-right">
       <li><a href="logout.php">Log Out </a></li>
